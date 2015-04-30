43b45d03519cc51229add3b0c115842add991a5f
HideAKi <hideaki.chain@gmail.com>
2015-04-30 11:33:23 +0900

    [WatchKit] sync CFBundleDisplayName with host app

--- lib/motion/project/template/ios-watch-extension-config.rb
+++ lib/motion/project/template/ios-watch-extension-config.rb
@@ -170,7 +170,8 @@ EOS
       #
       def merged_info_plist(platform)
         plist = super
-        plist['UIDeviceFamily'] = [4] # Probably means Apple Watch device?
+        plist['CFBundleDisplayName'] = ENV['RM_TARGET_HOST_APP_NAME']
+        plist['UIDeviceFamily'] << 4 # Probably means Apple Watch device?
         plist['WKWatchKitApp'] = true
         plist['WKCompanionAppBundleIdentifier'] = ENV['RM_TARGET_HOST_APP_IDENTIFIER']
         plist.delete('UIBackgroundModes')
